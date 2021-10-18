40c590fdf08080db8b8f7b3256ef39552a369e30
LinetTheFox <linetmoreandil@gmail.com>
2021-10-18 11:49:02 +0300

    Revert "Gleb: added already solved stuff"
    This reverts commit f5775ff74d345a8e4ebb17804ef8917faf13075e.

--- Gleb-hometask-13-10-21/21-remove-duplicates-from-sorted-array.c
+++
@@ -1,9 +0,0 @@
-int removeDuplicates(int* nums, int numsSize) {
-    int nextPos = 1;
-    for (int i = 0; i < numsSize; ++i) {
-        if (nums[i] != nums[nextPos - 1]) {
-            nums[nextPos++] = nums[i];
-        }
-    }
-    return numsSize ? nextPos : 0;
-}
