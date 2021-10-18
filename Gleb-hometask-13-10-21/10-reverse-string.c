5b49a93c9168793d2abd077f5be60456de9e1acb
LinetTheFox <linetmoreandil@gmail.com>
2021-10-18 11:49:01 +0300

    Revert "Gleb - task 10: removed memory completely :^)"
    This reverts commit cfcf02d71455cedfc4674bc35ad9da59275efd6a.

--- Gleb-hometask-13-10-21/10-reverse-string.c
+++ Gleb-hometask-13-10-21/10-reverse-string.c
@@ -1,8 +1,9 @@
 void reverseString(char* s, int sSize) {
+    char buffer = 0;
     // Apparently sSize doesn't include \0
     for (int begin = 0, end = sSize - 1; begin < end; ++begin, --end) {
-        s[begin] = s[begin] + s[end];
-        s[end] = s[begin] - s[end];
-        s[begin] = s[begin] - s[end];
+        buffer = s[end];
+        s[end] = s[begin];
+        s[begin] = buffer;
     }
 }
