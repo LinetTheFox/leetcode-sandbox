28faf0602ecd63415bf2ff0d781dcddb653795d3
LinetTheFox <linetmoreandil@gmail.com>
2021-10-18 11:49:02 +0300

    Revert "Gleb - added solution to task 10"
    This reverts commit d9cb03afbd3f47fab8c3e7616571d17e201fd584.

--- Gleb-hometask-13-10-21/10-reverse-string.c
+++
@@ -1,9 +0,0 @@
-void reverseString(char* s, int sSize) {
-    char buffer = 0;
-    // Apparently sSize doesn't include \0
-    for (int begin = 0, end = sSize - 1; begin < end; ++begin, --end) {
-        buffer = s[end];
-        s[end] = s[begin];
-        s[begin] = buffer;
-    }
-}
